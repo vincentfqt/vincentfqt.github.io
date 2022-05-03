aaed18c582ec05eaf64170feb5f8de69ac391e86
vincentfqt <89091045+vincentfqt@users.noreply.github.com>
2022-05-04 00:36:01 +0200

    Mise en place systeme envoi  mail contact

--- form.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-    if (!empty($_POST) && isset($_POST["submit"])) {
-        echo "OK";
-    } else {
-        echo "Erreur";
-    }
-
-?>
---
+++ js/index.js
@@ -0,0 +1,19 @@
+(function () {
+    emailjs.init('gBiK3iEzjXncKxQSV');
+})();
+
+window.onload = function () {
+    document.getElementById('contact-form').addEventListener('submit', function (event) {
+        event.preventDefault();
+        this.contact_number.value = Math.random() * 100000 | 0;
+
+        emailjs.sendForm('service_s49acgo', 'template_xnfqn7h', this)
+            .then(function () {
+                console.log('SUCCESS!');
+                alert('Merci pour votre message. Je reviens vers vous très vite !');
+            }, function (error) {
+                console.log('FAILED...', error);
+                alert('Une erreur est survenue, veuillez réessayer plus tard.');
+            });
+    });
+}
